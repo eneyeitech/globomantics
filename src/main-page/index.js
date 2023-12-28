7fdd3cee82b051400f6780bfd4254dbd869af052
Abdulmumin Eneye Abdulkarim <abdulmumineabdulkarim@gmail.com>
2023-12-28 17:02:40 +0100

    removed unused modules

--- src/main-page/index.js
+++ src/main-page/index.js
@@ -1,4 +1,4 @@
-import { useEffect, useMemo, useState } from "react";
+
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Header from "./header";
 import FeatureHouse from "./feature-house";
